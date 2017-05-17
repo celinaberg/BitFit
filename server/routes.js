296810c4e0e6a3b1c203b0ef2a17e02f698d970c
Rhys Bower <bowerrhys@gmail.com>
2017-05-17 12:23:43 -0700

    Upgrade sendfile api to sendFile

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@
 'use strict';
 
 var errors = require('./components/errors');
+var path = require('path');
 
 module.exports = function(app) {
 
@@ -18,7 +19,7 @@ module.exports = function(app) {
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
@@ -26,6 +27,6 @@ module.exports = function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
+      res.sendFile(path.join(app.get('appPath'), 'index.html'));
     });
 };
