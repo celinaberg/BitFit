bcd17cdca8bfae1960ff558119dab5958a25c02b
Rhys Bower <rhys.bower@alumni.ubc.ca>
2017-07-07 16:47:41 -0700

    Begin CWL layout

--- server/auth/cwl/index.js
+++ server/auth/cwl/index.js
@@ -6,8 +6,22 @@ var auth = require('../auth.service');
 
 var router = express.Router();
 
-router.post('/', function (req, res, next) {
-  passport.authenticate('local', function (err, user, info) {
+router.post('/callback',
+  passport.authenticate('saml', { failureRedirect: '/login/fail' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+router.post('/',
+  passport.authenticate('saml', { failureRedirect: '/login/fail' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+/*router.post('/', function (req, res, next) {
+  passport.authenticate('saml', function (err, user, info) {
     var error = err || info;
     if (error) return res.json(401, error);
     if (!user) return res.json(404, { message: 'Something went wrong, please try again.' });
@@ -15,6 +29,6 @@ router.post('/', function (req, res, next) {
     var token = auth.signToken(user._id, user.role);
     res.json({ token: token });
   })(req, res, next);
-});
+});*/
 
 module.exports = router;
---
+++ server/auth/cwl/passport.js
@@ -0,0 +1,40 @@
+var passport = require('passport');
+var SamlStrategy = require('passport-saml').Strategy;
+var fs = require('fs');
+var path = require('path');
+
+exports.setup = function (User, config) {
+  var samlStrategy = new SamlStrategy({
+    // URL that goes from the Identity Provider -> Service Provider
+    callbackUrl: 'https://127.0.0.1:4343/auth/local/callback',
+    // URL that goes from the Service Provider -> Identity Provider
+    entryPoint: 'https://authentication.ubc.ca',
+    // Usually specified as `/shibboleth` from site root
+    issuer: 'https://comped.cs.ubc.ca',
+    identifierFormat: null,
+    // Service Provider private key
+    decryptionPvk: fs.readFileSync(path.join(__dirname, '/../../cert/key.pem'), 'utf8'),
+    // Service Provider Certificate
+    privateCert: fs.readFileSync(path.join(__dirname, '/../../cert/key.pem'), 'utf8'),
+    // Identity Provider's public key
+    //cert: fs.readFileSync(path.join(__dirname, '/../../cert/idp_cert.pem'), 'utf8'),
+    validateInResponseTo: false,
+    disableRequestedAuthnContext: true
+  }, function(profile, done) {
+    User.findOne({
+      email: profile.email.toLowerCase()
+    }, function (err, user) {
+      if (err) return done(err);
+
+      if (!user) {
+        return done(null, false, { message: 'This student number is not registered.' });
+      }
+      if (!user.authenticate(password)) {
+        return done(null, false, { message: 'This password is not correct.' });
+      }
+      return done(null, user);
+    });
+  });
+
+  passport.use(samlStrategy);
+};
