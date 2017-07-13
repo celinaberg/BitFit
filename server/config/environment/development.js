0929eb86c7f45c159866755f6a3a1ea586d5fe05
Rhys Bower <rhys.bower@alumni.ubc.ca>
2017-07-12 20:13:44 -0700

    Add config for CWL production

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,15 @@ module.exports = {
 
   ip: '127.0.0.1',
 
-  port: 4343,
+  httpsPort: 4343,
+  httpPort: 8085,
+
+  url: 'https://localhost:4343',
+
+  // CWL
+  callbackUrl: 'https://localhost:4343/auth/cwl/login/callback',
+  entryPoint: 'https://authentication.stg.id.ubc.ca/idp/profile/SAML2/Redirect/SSO',
+  idpCert: path.join(__dirname, '/../../..', 'server/cert/idp_cert-staging.pem'),
 
   // MongoDB connection options
   mongo: {
