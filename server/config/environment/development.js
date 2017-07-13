52a4b84900be904bceda2b63ebe787da3b8d695e
Rhys Bower <rhys.bower@alumni.ubc.ca>
2017-07-12 20:51:25 -0700

    Fix CWL staging callback url

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var path = require('path');
+
 // Development specific configuration
 // ==================================
 module.exports = {
@@ -7,12 +9,12 @@ module.exports = {
   ip: '127.0.0.1',
 
   httpsPort: 4343,
-  httpPort: 8085,
+  httpPort: 8080,
 
   url: 'https://localhost:4343',
 
   // CWL
-  callbackUrl: 'https://localhost:4343/auth/cwl/login/callback',
+  callbackUrl: 'https://127.0.0.1:4343/auth/cwl/login/callback',
   entryPoint: 'https://authentication.stg.id.ubc.ca/idp/profile/SAML2/Redirect/SSO',
   idpCert: path.join(__dirname, '/../../..', 'server/cert/idp_cert-staging.pem'),
 
