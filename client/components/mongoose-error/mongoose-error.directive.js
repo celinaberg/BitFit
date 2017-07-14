f1e95d366a731f30327c0c636c2414c4034e7d11
Rhys Bower <rhys.bower@alumni.ubc.ca>
2017-07-13 21:29:58 -0700

    Update mongoose error module name

--- client/components/mongoose-error/mongoose-error.directive.js
+++ client/components/mongoose-error/mongoose-error.directive.js
@@ -3,7 +3,7 @@
 /**
  * Removes server error when user updates input
  */
-angular.module('its110App')
+angular.module('bitfit.directives.mongoose-error')
   .directive('mongooseError', function () {
     return {
       restrict: 'A',
