d9d560cc74a4b9524aa287f96d81e5b5bbd3999c
Rhys Bower <rhys.bower@alumni.ubc.ca>
2017-08-09 22:27:30 -0700

    Make redux store immutable

--- react-client/src/types.js
+++ react-client/src/types.js
@@ -5,67 +5,67 @@ import RichTextEditor from 'react-rte';
 export type Role = "student" | "teaching-assistant" | "instructor";
 
 export type User = {
-  id: string,
-  uid: string,
-  firstName: string,
-  lastName: string,
-  displayName: string,
-  role: Role,
-  studentNumber: ?string,
-  employeeNumber: ?string,
-  section: ?string,
-  term: ?number,
-  session: ?string,
-  year: ?number
+  +id: string,
+  +uid: string,
+  +firstName: string,
+  +lastName: string,
+  +displayName: string,
+  +role: Role,
+  +studentNumber: ?string,
+  +employeeNumber: ?string,
+  +section: ?string,
+  +term: ?number,
+  +session: ?string,
+  +year: ?number
 };
 
 export type Lesson = {
-  id: string,
-  title: string,
-  background: string,
-  questions: Array<Question>
+  +id: string,
+  +title: string,
+  +background: string,
+  +questions: Array<Question>
 };
 
 export type Question = {
-  id: string,
-  title: string,
-  instructions: string,
-  code: string,
-  className: string,
-  readOnly: bool,
-  hints: Array<string>,
-  tags: string,
-  expectedOutput: string,
-  topic: Lesson
+  +id: string,
+  +title: string,
+  +instructions: string,
+  +code: string,
+  +className: string,
+  +readOnly: bool,
+  +hints: Array<string>,
+  +tags: string,
+  +expectedOutput: string,
+  +topic: Lesson
 };
 
 export type UserState = {
-  fetching: bool,
-  fetched: bool,
-  users: Array<User>,
-  error: null
+  +fetching: bool,
+  +fetched: bool,
+  +users: Array<User>,
+  +error: null
 }
 
 export type LessonState = {
-  fetching: bool,
-  fetched: bool,
-  lessons: Array<Lesson>,
-  error: null,
-  new: {
-    title: string,
-    background: RichTextEditor
+  +fetching: bool,
+  +fetched: bool,
+  +lessons: Array<Lesson>,
+  +error: null,
+  +new: {
+    +title: string,
+    +background: RichTextEditor
   }
 }
 
 export type QuestionState = {
-  fetching: bool,
-  fetched: bool,
-  questions: Array<Question>,
-  error: null
+  +fetching: bool,
+  +fetched: bool,
+  +questions: Array<Question>,
+  +error: null
 }
 
 export type State = {
-  users: UserState,
-  lessons: LessonState,
-  questions: QuestionState
+  +users: UserState,
+  +lessons: LessonState,
+  +questions: QuestionState
 }
