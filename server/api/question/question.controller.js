95ccdf5d8010807466c162d8bb0ef64360d480cb
Rhys Bower <rhys.bower@alumni.ubc.ca>
2017-08-25 19:05:44 -0700

    Get most of the lesson selector working

--- server/api/question/question.controller.js
+++ server/api/question/question.controller.js
@@ -4,15 +4,47 @@ import type { $Request, $Response } from "express";
 
 import _ from "lodash";
 import Question from "./question.model";
+import Lesson from "../lesson/lesson.model";
 
-function handleError(res, err) {
+function handleError(res: $Response, err: Error) {
+  console.trace();
+  console.error(err);
   return res.status(500).send(err);
+}
+
+async function updateLesson(res: $Response, question: Question) {
+  console.log("in updateLesson");
+  if (question.lesson) {
+    try {
+      let lesson: Lesson = await Lesson.findById(question.lesson);
+      console.log(lesson);
+      if (!lesson.questions.includes(question.id)) {
+        console.log("question id: ", question.id);
+        console.log("lesson id: ", question.lesson);
+        console.log("loaded lesson", lesson.id);
+        lesson.questions.push(question.id);
+        console.log(lesson);
+        await lesson.save();
+      }
+    } catch (err) {
+      handleError(res, err);
+    }
+  }
 }
 
 // Get list of questions
 export async function index(req: $Request, res: $Response) {
   try {
-    const questions = await Question.find();
+    let questions = await Question.find();
+    for (let i = 0; i < questions.length; i++) {
+      const question = questions[i];
+      const lesson = await question.lesson();
+      console.log("lesson", lesson);
+      let returnQuestion = question.toObject();
+      returnQuestion["lesson"] = lesson;
+      console.log(returnQuestion);
+      questions[i] = returnQuestion;
+    }
     return res.status(200).json(questions);
   } catch (err) {
     return handleError(res, err);
@@ -20,36 +52,36 @@ export async function index(req: $Request, res: $Response) {
 }
 
 // Get list of questions for export
-export function exportQuestions(req: $Request, res: $Response) {
-  Question.find({}, { _id: 0, topic: 0 }, (err, questions) => {
-    if (err) {
-      return handleError(res, err);
-    }
+export async function exportQuestions(req: $Request, res: $Response) {
+  try {
+    const questions = await Question.find({}, { _id: 0, topic: 0 });
     return res.status(200).json(questions);
-  });
+  } catch (err) {
+    return handleError(res, err);
+  }
 }
 
 // Get a single question
-export function show(req: $Request, res: $Response) {
-  Question.findById(req.params.id, (err, question) => {
-    if (err) {
-      return handleError(res, err);
-    }
+export async function show(req: $Request, res: $Response) {
+  try {
+    const question = await Question.findById(req.params.id);
     if (!question) {
       return res.sendStatus(404);
     }
     return res.json(question);
-  });
+  } catch (err) {
+    return handleError(res, err);
+  }
 }
 
 // Creates a new question in the DB.
-export function create(req: $Request, res: $Response) {
-  Question.create(req.body, (err, question) => {
-    if (err) {
-      return handleError(res, err);
-    }
+export async function create(req: $Request, res: $Response) {
+  try {
+    const question = await Question.create(req.body);
     return res.status(201).json(question);
-  });
+  } catch (err) {
+    return handleError(res, err);
+  }
 }
 
 // Import many questions to DB.
@@ -63,26 +95,22 @@ export function importQuestions(req: $Request, res: $Response) {
 }
 
 // Updates an existing question in the DB.
-export function update(req: $Request, res: $Response) {
-  if (req.body._id) {
-    delete req.body._id;
-  }
-  Question.findById(req.params.id, (err, question) => {
-    if (err) {
-      return handleError(res, err);
-    }
+export async function update(req: $Request, res: $Response) {
+  console.log("in update");
+  const newQuestion = req.body;
+  try {
+    let question: Question = await Question.findById(req.params.id);
     if (!question) {
       return res.sendStatus(404);
     }
-    question.hints = req.body.hints; // manually copy as merge doesn't
-    const updated = _.merge(question, req.body);
-    updated.save(err => {
-      if (err) {
-        return handleError(res, err);
-      }
-      return res.status(200).json(question);
-    });
-  });
+    question.hints = newQuestion.hints; // manually copy as merge doesn't
+    const updated = _.merge(question, newQuestion);
+    await updateLesson(res, newQuestion);
+    await updated.save();
+    return res.status(200).json(question);
+  } catch (err) {
+    return handleError(res, err);
+  }
 }
 
 // Deletes a question from the DB.
--- server/api/question/question.model.js
+++ server/api/question/question.model.js
@@ -1,6 +1,7 @@
 // @flow
 
 import mongoose from "mongoose";
+import Lesson from "../lesson/lesson.model";
 
 const Schema = mongoose.Schema;
 
@@ -15,12 +16,30 @@ const QuestionSchema = new Schema({
   expectedOutput: String
 });
 
-QuestionSchema.methods.toJSON = function() {
-  var obj = this.toObject();
-  obj.id = obj._id;
-  delete obj._id;
-  delete obj.__v;
-  return obj;
+QuestionSchema.methods.lesson = async function() {
+  console.log("get virtual lesson");
+  console.log(this);
+  console.log(this._id);
+  try {
+    const lesson = await Lesson.findOne({
+      questions: {
+        $in: [mongoose.Types.ObjectId(this._id)]
+      }
+    });
+    console.log("found", lesson);
+    return lesson.id;
+  } catch (err) {
+    console.log("not found");
+    return null;
+  }
+};
+
+QuestionSchema.options.toObject = {};
+QuestionSchema.options.toObject.transform = function(doc, ret, options) {
+  ret.id = ret._id;
+  delete ret._id;
+  delete ret.__v;
+  return ret;
 };
 
 export default mongoose.model("Question", QuestionSchema);
