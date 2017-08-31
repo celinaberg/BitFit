33f230cb15f2809ed43ec06179cc0d4ed2101b7c
Rhys Bower <rhys.bower@alumni.ubc.ca>
2017-08-31 14:40:51 -0700

    Remove service worker

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
